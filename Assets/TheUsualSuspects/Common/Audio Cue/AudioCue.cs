58ced866c0d835d436aeb3234e54181b5411d0f3
Paulo Oliveira <paulo@ninito.me>
2021-08-04 23:41:35 -0300

    Add AudioCue tool

---
+++ Assets/TheUsualSuspects/Common/Audio Cue/AudioCue.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Ninito.UsualSuspects.AudioCue
+{
+    /// <summary>
+    ///     A collection of audio clips with configurable settings
+    /// </summary>
+    [CreateAssetMenu(order = CreateAssetMenus.AudioCueOrder, fileName = CreateAssetMenus.AudioCueFileName,
+        menuName = CreateAssetMenus.AudioCueMenuName)]
+    public class AudioCue : ScriptableObject
+    {
+        #region Private Fields
+
+        [Header("Audio Cue Parameters")]
+        [SerializeField]
+        private AudioClip[] clips;
+
+        [Header("Pitch"), SerializeField]
+        private float basePitch;
+
+        [SerializeField]
+        private float pitchDeviation;
+
+        #endregion
+
+        #region Public Methods
+
+        public (AudioClip, float) GetClipWithPitch()
+        {
+            return (GetClip(), GetPitch());
+        }
+
+        public AudioClip GetClip()
+        {
+            return clips[Random.Range(0, clips.Length)];
+        }
+
+        public float GetPitch()
+        {
+            return basePitch + Random.Range(pitchDeviation * -1, pitchDeviation);
+        }
+
+        #endregion
+    }
+}
--- Assets/TheUsualSuspects/CreateAssetMenus.cs
+++ Assets/TheUsualSuspects/CreateAssetMenus.cs
@@ -8,36 +8,24 @@
         #region UI Page Settings
 
         public const int UIPageSettingsOrder = 0;
-        public const string UIPageSettingsMenuName = "ManyTools/UnityExtended/UI/Page Settings";
+        public const string UIPageSettingsMenuName = "Ninito/The Usual Suspects/UI/Page Settings";
         public const string UIPageSettingsFileName = "UI Page Settings";
-
-        #endregion
-
-        #region Scriptable Enums
-
-        public const int ScriptableEnumOrder = 0;
-        public const string ScriptableEnumMenuName = "ManyTools/UnityExtended/Scriptable Enum";
-        public const string ScriptableEnumFileName = "Scriptable Enum";
-        
-        public const int DamageTypeOrder = 10;
-        public const string DamageTypeMenuName = "ManyTools/UnityExtended/Damage Type";
-        public const string DamageTypeFileName = "Damage Type";
-        
-        #endregion
-
-        #region Runtime Sets
-
-        public const int GameObjectRuntimeSetOrder = 0;
-        public const string GameObjectRuntimeSetMenuName = "ManyTools/UnityExtended/Runtime Sets/GameObject Runtime Set";
-        public const string GameObjectRuntimeSetFileName = "GameObject Runtime Set";
 
         #endregion
 
         #region Damageable
 
-        public const int DamageProcessorOrder = 0;
-        public const string DamageProcessorMenuName = "ManyTools/UnityExtended/Damageable/Damage Processor";
-        public const string DamageProcessorFileName = "Damage Processor";
+        public const int DamageTypeOrder = 10;
+        public const string DamageTypeMenuName = "Ninito/The Usual Suspects/Damage Type";
+        public const string DamageTypeFileName = "Damage Type";
+
+        #endregion
+
+        #region Audio Cue
+
+        public const int AudioCueOrder = 10;
+        public const string AudioCueMenuName = "Ninito/The Usual Suspects/Audio Cue";
+        public const string AudioCueFileName = "Audio Cue";
 
         #endregion
     }
