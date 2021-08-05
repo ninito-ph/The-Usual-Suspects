aed84f0d1fbe8d1a6f91e9f99cdcf476c23c7eb5
Paulo Oliveira <paulo@ninito.me>
2021-08-05 19:39:00 -0300

    Rename FlexibleLayoutGroup enum and add OnValidate method

--- Assets/TheUsualSuspects/Common/Flexible Layout Group/FlexibleLayoutGroup.cs
+++ Assets/TheUsualSuspects/Common/Flexible Layout Group/FlexibleLayoutGroup.cs
@@ -6,7 +6,8 @@ namespace Ninito.UsualSuspects.FlexibleLayout
     /// <summary>
     ///     A flexible layout capable of imitating grid, horizontal and vertical layout, with high customizability
     /// </summary>
-    public class FlexibleLayout : LayoutGroup
+    [ExecuteInEditMode]
+    public class FlexibleLayoutGroup : LayoutGroup
     {
         #region Private Fields
 
@@ -57,11 +58,23 @@ namespace Ninito.UsualSuspects.FlexibleLayout
         public override void SetLayoutVertical()
         {
         }
-        
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            ClampRowAndColumnValues();
+        }
+
         #endregion
 
         #region Private Methods
 
+        private void ClampRowAndColumnValues()
+        {
+            rows = Mathf.Max(1, rows);
+            columns = Mathf.Max(1, columns);
+        }
+        
         /// <summary>
         ///     Calculates and sets the row and column count, according to the fit type
         /// </summary>
@@ -81,14 +94,14 @@ namespace Ninito.UsualSuspects.FlexibleLayout
 
         private int GetColumnCount(int childCount, float squareRoot)
         {
-            return fitType == FitType.Height || fitType == FitType.FixedRows
+            return fitType == FitType.Width || fitType == FitType.FixedRows
                 ? Mathf.CeilToInt(childCount / (float) rows)
                 : Mathf.CeilToInt(squareRoot);
         }
 
         private int GetRowCount(int childCount, float squareRoot)
         {
-            return fitType == FitType.Width || fitType == FitType.FixedColumns
+            return fitType == FitType.Height || fitType == FitType.FixedColumns
                 ? Mathf.CeilToInt(childCount / (float) columns)
                 : Mathf.CeilToInt(squareRoot);
         }
@@ -158,8 +171,8 @@ namespace Ninito.UsualSuspects.FlexibleLayout
         private enum FitType
         {
             Uniform,
-            Width,
             Height,
+            Width,
             FixedRows,
             FixedColumns
         }
